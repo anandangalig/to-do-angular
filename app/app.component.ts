a2ab9ffa7089786b0cd6efe259ce92a748bb8a23
Anand Angalig <anandangalig@gmail.com>
2016-10-17 16:05:19 -0400

    add Angular setup files and gulpfile.js

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,14 @@
+import { Component } from '@angular/core'; //pulls in extra functionality and definitions that our files need. Same as require for gulpfile and <script> for html.
+
+// Component Annotation section:==================================
 @Component({
   selector: 'my-app',
   template: `
   <h1>My First Angular 2 App</h1>
   `
 })
+// Component Class Declaration section: includes the functionality of the component
+
+export class AppComponent {
+
+} // exports our components and makes it available for other components to use. 
