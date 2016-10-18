013aeb96b6b5bd76d0f515267bbea70843c1f827
Anand Angalig <anandangalig@gmail.com>
2016-10-18 10:55:35 -0400

    continue with To-Do assignment, add basic event listener

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,46 +6,26 @@ import { Component } from '@angular/core'; //pulls in extra functionality and de
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3>One of my favorite bands is: {{ favoriteBand }}</h3>
-    <p>If I had to choose a favorite painter it would be: {{ favoritePainter }}</p>
-    <p>The number of slices of pie I would like is: {{ slicesOfPie }}</p>
-    <h3>One of my favorite albums is: </h3>
-    <p>{{ favoriteAlbum.title }}</p>
-    <p>By {{ favoriteAlbum.artist }}</p>
-    <p>Released in {{ favoriteAlbum.released }}</p>
-    <h3>Here are my favorite pies!</h3>
-    <div class="pie" *ngFor="let currentPie of favoritePies">
-      <p>{{currentPie}}</p>
-    </div>
-    <h3>Here are my favorite albums!</h3>
-    <div class="album" *ngFor="let album of albums">
-      <p>{{album.title}}</p>
-      <p>By {{album.artist}}</p>
-      <p>Released in {{album.released}}</p>
-    </div>
+    <h3 (click)="doStuff()" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
   </div>
   `
 })
 // Component Class Declarations section: includes the functionality of the component. When the HTML gets rendered, these Class exports provide the objects and the models.
 
 export class AppComponent {
-  favoriteBand: string = 'Ani DiFranco';
-  favoritePainter: string = 'Van Gogh';
-  slicesOfPie: number = 3;
-  favoriteAlbum: Album = new Album("Disintegration", "The Cure", 1989);
-  favoritePies: string[] = ["Apple", "Banana Cream", "Blackberry"];
-  albums: Album[] = [
-      new Album("Pulse", "Pink Floyd", 1995),
-      new Album("Funhouse", "The Stooges", 1970),
-      new Album("Twilight of the Thunder God", "Amon Amarth", 2008),
-      new Album("Dilate", "Ani DiFranco", 1996),
-      new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
-      new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
-    ];
+  public tasks: Task[] = [
+    new Task("Finish Angular To-Do.", 0),
+    new Task("Read about Pipe.", 1),
+    new Task("Attend ITEM Meetup.", 2),
+    new Task("Eat Lunch.", 3)
+  ];
+  doStuff() {
+    alert("A task was clicked on!");
   }
+}
 
-
-export class Album {
-  constructor (public title: string, public artist: string, public released: number) {  } //creating this Class and using it on line 25 to export the album in case we have many albums.
-
-} // exports our components and makes it available for other components to use.
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id:number) {  }
+}
+// exports our components and makes it available for other components to use.
