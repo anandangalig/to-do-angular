eed97c01e1bfd18f33e4650d321f4a9247deccbf
Anand Angalig <anandangalig@gmail.com>
2016-10-18 13:47:41 -0400

    add *ngIf to show/hide edit forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core'; //pulls in extra functionality and de
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="showDetails(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
-    <h1>Edit Task</h1>
+    <div *ngFor="let currentTask of tasks">
+      <h3>{{ currentTask.description }}</h3>
+      <button (click)="showDetails(currentTask)">Edit</button>
+    </div>
+    <div *ngIf="selectedTask">
+      <h1>Edit Task</h1>
       <div>
         <label>Enter Task Description:</label>
         <input [(ngModel)]="selectedTask.description">
@@ -15,7 +19,9 @@ import { Component } from '@angular/core'; //pulls in extra functionality and de
       <div>
         <label>Enter Task ID:</label>
         <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
       </div>
+    </div>
   </div>
   `
 })
@@ -28,9 +34,13 @@ export class AppComponent {
     new Task("Attend ITEM Meetup.", 2),
     new Task("Eat Lunch.", 3)
   ];
-  selectedTask: Task = this.tasks[0]; //?????WHY MAKE IT 0th INDEX????????
+  //selectedTask: Task = this.tasks[0]; this is just setting the default, so ngModel latches to it right away-its job.
+  selectedTask: Task = null; // here, the null makes the default item unselected, as it should be since it will be set by each edit button belonging to each task.
   showDetails(clickedTask: Task) { // now this method will expect a Task object to be passed whenever it is called.
     this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
